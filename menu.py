adec6b95afc5f7933c9917b6f76f46a52c3a67f7
Ariel Junior <act117@einstein.br>
2023-05-02 20:14:19 -0300

    melhorias em consultar pessoas

--- menu.py
+++ menu.py
@@ -1,6 +1,36 @@
 from person import Person
 import repository
 
+def getLineTable(valuesColumns = [], isHead = False, max_width = 20):
+    if len(valuesColumns) == 0:
+        return ""
+    
+    result = ""
+    widthBorder = -1
+    for columnName in valuesColumns:
+        myColumn = f"| {columnName} "
+
+        if len(myColumn) < max_width:
+            diff = max_width - len(myColumn)
+            myColumn += " " * diff
+        elif len(myColumn) > max_width:
+            myColumn = myColumn[:max_width]
+
+        widthBorder += len(myColumn)
+        result += myColumn
+
+    result += "|"
+
+    if isHead == False:
+        return result
+
+    borderTop = " " + ("_" * widthBorder)
+    borderBottom = " " + ("¨" * widthBorder)
+
+    result = f"{borderTop}\n{result}\n{borderBottom}"
+
+    return result
+
 opcao = ""
 while opcao != "3":
     print("\n1 - consultar pessoas\n2 - cadastrar pessoa\n3 - sair")
@@ -8,9 +38,14 @@ while opcao != "3":
     match opcao:
         case '1':
             print("----CONSULTAR PESSOAS---\n")
+            
             persons = repository.getAllPersons();
+            headerTable = getLineTable(Person.getColumnsName(), True)
+
+            print(headerTable)
             for person in persons:
-                print(person.toString())
+                line = getLineTable([person.fullname, person.datebirth, person.email])
+                print(line)
         case '2':
             print("----CADASTRAR PESSOA---\n")
 
--- person.py
+++ person.py
@@ -4,10 +4,14 @@ class Person:
         self.datebirth = datebirth
         self.email = email
     
-    def toString(_self):
+    def __str__(_self):
         return f" NOME COMPLETO = {_self.fullname}\n DATA DE ANIVERSÁRIO = {_self.datebirth}\n EMAIL = {_self.email}\n"
     
     def getLineToCSV(_self):
         return "\n" + ",".join([_self.fullname, _self.datebirth, _self.email])
+    
+    @staticmethod
+    def getColumnsName():
+        return ["NOME COMPLETO", "DATA ANIVERSÁRIO", "EMAIL"]
     
 # print(Person("ariel junior", "17/05/1996", "ariel@teste.com"))
